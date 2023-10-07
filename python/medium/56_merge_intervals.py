a2fe09d74ceb21df276820d7558331e5965e88cf
sfumato <sfumato000@gmail.com>
2023-10-07 12:47:51 -0700

    Add Space O(1) solution for LC 56_merge_intervals

--- python/medium/56_merge_intervals.py
+++ python/medium/56_merge_intervals.py
@@ -1,15 +1,27 @@
+from typing import List
+
+
 class Solution:
+    """Time: O(NLogN), Space: O(1)"""
+
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals:
             return []
 
-        intervals.sort(key=lambda x: x[0])
-        out = [intervals[0]]
+        intervals.sort(key=lambda x: x[1])
+        n = len(intervals)
+        prev, last = n - 1, n - 1
 
-        for start, end in intervals[1:]:
-            if start <= out[-1][1]:
-                out[-1][1] = max(out[-1][1], end)
-            else:
-                out.append([start, end])
+        for i in range(n - 2, -1, -1):
+            if intervals[prev][0] <= intervals[i][1]:
+                intervals[i][:] = (
+                    min(intervals[prev][0], intervals[i][0]),
+                    intervals[prev][1],
+                )
+                intervals[prev][:] = intervals[last]
+                last -= 1
+                intervals.pop()
+            prev = i
 
-        return out
+        intervals.sort()
+        return intervals
